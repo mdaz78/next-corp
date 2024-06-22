60ee770f87f14534f5f2592fd19caa5b56fb5610
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-22 11:45:27 +0530

    create a header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href={'/performance'}>Performance</Link>
-        <Link href={'/reliability'}>Reliability</Link>
-        <Link href={'/scale'}>Scale</Link>
-      </div>
+      <div>Home Page</div>
     </div>
   );
 }
